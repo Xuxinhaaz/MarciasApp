d6adc07875d01dc93c52fe865d8ff7e30497c5c6
joojjunu <123701961+joojjunu@users.noreply.github.com>
2024-08-21 10:57:34 -0300

    Frontend Commit

---
+++ MarciasFrontEnd/MarciasFrontEnd/src/app/Pages/MarketPage/market-home/market-home.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { MktHeaderComponent } from "../../../Components/mktHeader/mkt-header/mkt-header.component";
+import { FoodsComponent } from '../../../Components/foods/foods.component';
+import { ViewportScroller } from '@angular/common';
+import { RecipesList } from '../../../Recipes';
+
+@Component({
+  selector: 'app-market-home',
+  standalone: true,
+  imports: [MktHeaderComponent, FoodsComponent],
+  templateUrl: './market-home.component.html',
+  styleUrl: './market-home.component.scss'
+})
+export class MarketHomeComponent {
+  foods= RecipesList;
+}
+
